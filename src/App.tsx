cc29b5efa42e3dbea8187e354c0e37c19674f556
Luka Djordjevic <ldjordjevic@protonmail.com>
2026-05-09 12:22:28 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="size-35 bg-red-500"></div>
+  )
+}
+
+export default App
+
+
+// styling Tailwind
+// data fetching - TanStack Quer cky (React query)
