a87d7fd0f45115318c5e58787d8ba41357774040
Luka Djordjevic <ldjordjevic@protonmail.com>
2026-05-09 14:22:11 +0200

    feat: Add zod and zod schemas for weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useQuery } from "@tanstack/react-query"
+import { getWeather } from "./api"
+
 function App() {
 
+  const { data } = useQuery({
+    queryKey: ['weather'],
+    queryFn: () => getWeather({ lat: 10, lon: 25 })
+  })
+
   return (
-    <div className="size-35 bg-red-500"></div>
+    <>{JSON.stringify(data)}</>
   )
 }
+
 
 export default App
 
