c601fe330e954eccd721777a9d215ee485c1507e
Luka Djordjevic <ldjordjevic@protonmail.com>
2026-05-11 10:51:33 +0200

    feat: Introduce Location schema

---
+++ src/schemas/locationSchema.ts
@@ -0,0 +1,11 @@
+import z from 'zod'
+
+export const LocationSchema = z.array(
+  z.object({
+    name: z.string(),
+    lat: z.number(),
+    lon: z.number(),
+    country: z.string(),
+    state: z.string().optional(),
+  })
+)
